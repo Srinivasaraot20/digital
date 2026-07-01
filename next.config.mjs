bbc482d268fafba7f4da713c938e77a0a1fe2fe6
Srinivasa Rao Talari <143414405+Srinivasaraot20@users.noreply.github.com>
2026-07-01 12:27:35 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  turbopack: {},
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'core-js/modules/es.array.at': false,
+        'core-js/modules/es.array.flat': false,
+        'core-js/modules/es.object.from-entries': false,
+        'core-js/modules/es.object.has-own': false,
+        'core-js/modules/es.string.trim-start': false,
+        'core-js/modules/es.string.trim-end': false,
+      };
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
